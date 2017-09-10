1eb531f0a29a726278ba198fc6d5e233dc0d9dcb
wxf22105 <mihaid22105@gmail.com>
2017-09-10 13:37:07 +0300

    Initial commit

---
+++ app/src/main/java/com/wxf/helloworld/XMyActivity.java
@@ -0,0 +1,13 @@
+package com.wxf.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class XMyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xmy);
+    }
+}
